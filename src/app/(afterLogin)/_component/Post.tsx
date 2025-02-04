ff474d6748b3587a00e12dd5879cdc907e10be8a
yeonhwa <yeonhwa6717@gmail.com>
2025-02-04 16:17:15 +0900

    post, profile detail - 부모 클라이언트컴포넌트, 자식 서버컴포넌트 props나 children으로 넘기기, import하면 클라이언트 컴포넌트됨

--- src/app/(afterLogin)/_component/Post.tsx
+++ src/app/(afterLogin)/_component/Post.tsx
@@ -4,12 +4,19 @@ import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import 'dayjs/locale/ko';
 import ActionButtons from "@/app/(afterLogin)/_component/ActionButtons";
+import PostArticle from './PostArticle';
+import { faker } from '@faker-js/faker';
+import PostImages from "@/app/(afterLogin)/_component/PostImages";
 
 dayjs.locale('ko');
 dayjs.extend(relativeTime)
 
-export default function Post() {
+type Props = {
+  noImage?: boolean
+}
+export default function Post({ noImage }: Props) {
   const target = {
+    postId: 1,
     User: {
       id: 'elonmusk',
       nickname: 'Elon Musk',
@@ -17,10 +24,20 @@ export default function Post() {
     },
     content: '클론코딩 라이브로 하니 너무 힘들어요 ㅠㅠ',
     createdAt: new Date(),
-    Images: [],
+    Images: [] as any[],
   }
+
+  if (Math.random() > 0.5 && !noImage) {
+    target.Images.push(
+      { imageId: 1, link: faker.image.urlLoremFlickr() },
+      { imageId: 2, link: faker.image.urlLoremFlickr() },
+      { imageId: 3, link: faker.image.urlLoremFlickr() },
+      { imageId: 4, link: faker.image.urlLoremFlickr() },
+    )
+  }
+
   return (
-    <article className={style.post}>
+    <PostArticle post={target}>
       <div className={style.postWrapper}>
         <div className={style.postUserSection}>
           <Link href={`/${target.User.id}`} className={style.postUserImage}>
@@ -41,12 +58,12 @@ export default function Post() {
             <span className={style.postDate}>{dayjs(target.createdAt).fromNow(true)}</span>
           </div>
           <div>{target.content}</div>
-          <div className={style.postImageSection}>
-
+          <div>
+            <PostImages post={target} />
           </div>
           <ActionButtons />
         </div>
       </div>
-    </article>
+    </PostArticle>
   )
 }
