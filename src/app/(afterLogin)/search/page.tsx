ef2752b299f2edc0b464d3d4ec15b48c6dc6555d
yeonhwa <yeonhwa6717@gmail.com>
2025-02-04 15:54:36 +0900

    profile, search - seachParam, useSearchParma으로 쿼리

--- src/app/(afterLogin)/search/page.tsx
+++ src/app/(afterLogin)/search/page.tsx
@@ -1,3 +1,40 @@
-export default function SearchPage() {
-  return <>SEARCH</>
+import style from './search.module.css';
+import BackButton from "@/app/(afterLogin)/_component/BackButton";
+import SearchForm from "@/app/(afterLogin)/_component/SearchForm";
+import Tab from "@/app/(afterLogin)/search/_component/Tab";
+import Post from "@/app/(afterLogin)/_component/Post";
+type Props = {
+  searchParams: Promise<{ q: string, f?: string, pf?: string }>;
+}
+export default async function SearchPage({ searchParams }: Props) {
+  const { q } = await searchParams;
+  return (
+    <main className={style.main}>
+      <div className={style.searchTop}>
+        <div className={style.searchZone}>
+          <div className={style.buttonZone}>
+            <BackButton />
+          </div>
+          <div className={style.formZone}>
+            <SearchForm q={q} />
+          </div>
+        </div>
+        <Tab />
+      </div>
+      <div className={style.list}>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        {/*<SearchResult searchParams={searchParams} />*/}
+      </div>
+    </main>
+  )
 }
