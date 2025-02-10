e6fda04e4e061d86fb635f7f11e5811c29a82dda
yeonhwa <yeonhwa6717@gmail.com>
2025-02-10 15:00:44 +0900

    이미지 캐싱

--- src/app/(afterLogin)/home/_lib/getPostRecommends.tsx
+++ src/app/(afterLogin)/home/_lib/getPostRecommends.tsx
@@ -3,7 +3,8 @@ export async function getPostRecommends({ pageParam }: Props) {
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/postRecommends?cursor=${pageParam}`, {
     next: {
       tags: ['posts', 'recommends']
-    }
+    },
+    cache: 'force-cache',
   });
   if (!res.ok) {
     throw new Error('Failed to fetch data')
